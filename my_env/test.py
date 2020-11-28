0ea72b47f585bda5a80f8ae7bd8c7f97be446e1f
Jherson <admedinaco@unal.edu.co>
2020-11-28 12:33:26 -0500

    adding functions

--- my_env/test.py
+++ my_env/test.py
@@ -3,43 +3,49 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 #Modified Prefix Structure for Natural Language Processing
+#Modified Prefix Structure for Natural Language Processing
 
 #-------------GLOBAL VARIABLES------------------------------------------------------------
 node = 0 #Number of nodes, the root node is node 0
 tree = [{}] #Tree itself
-valWord = [{}] #Set of the most frequent valid words
-freq = [{}] #Frecuency of Node i
-isWord = [{}] #Boolean value set true if the current node is a complete word
+valWord = {} #Set of the most frequent valid words
+freq = [] #Frecuency of Node i
+isWord = [] #Boolean value set true if the current node is a complete word
 word = "" #Current word that it's written
+tree.append([{}])
+freq.append(1)
+isWord.append(False)
 #-----------------------------------------------------------------------------------------
 
 #------------SEARCHING AND ADDING FUNCTIONS-----------------------------------------------
 def find(curnode, curword): #Searhces for all valid words
-    if curnode in isWord: #If current node is an actual word
-        valWord[curword] = freq[curnode]
-    for nxt in tree[curnode]: #Recursively finds other valid words
-        find(nxt[0], curword + nxt[1])
-    return
+  global isWord
+  if isWord[curnode]: #If current node is an actual word
+    valWord[curword] = [freq[curnode], curnode]
+  for nxt in tree[curnode]: #Recursively finds other valid words
+    find(nxt[1], curword + nxt[0])
+  return
 def add(c): #Updates the 'word' by appending 'c' to the end, and searches for new valid words
-    if not isalpha(c):
-        freq[node] += 1
-        isWord[node] = True
-        word = ""
-        #Clean text box
-        return
-    if c in tree[node]:
-        node = tree[node][c]
-        word += c
-    else :
-        tree[node][c] = node + 1
-        node += 1
-        tree[node] = {}
-        freq[node] = 1
-        isWord[node] = False
-        word += c
-    valWord.clear()
-    find(node, word)
+  global node
+  global word
+  if not c.isalpha():
+    freq[node] += 1
+    isWord[node] = True
+    word = ""
     return
+  if c in tree[node]:
+    node = tree[node][c]
+    word += c
+  else :
+    tree[node][c] = node + 1
+    node += 1
+    tree.append({})
+    freq.append(1)
+    isWord.append(False)
+    word += c
+  valWord.clear()
+  #find(node, word)
+  return
 
 @app.route('/')
 def index():
@@ -49,15 +55,21 @@ def index():
 def my_form_post():
 	text = request.form['input']
 	processed_text = text.upper()
-	print(processed_text)
-	if len(word) > 1:
-		mostFreq = sorted(valWord) #Sorts the valwords by their frequency
-		need = 5 #Prints up to 5 words
-		for i in mostFreq: #From the most frequent one to the least frequent one
-			if need == 0: #5 words have are already been printed
-				break
-				#print(i[0])
-				need -= 1
-
+	for c in processed_text:
+		add(c)
+	global node
+	global word
+	global tree
+	global isWord
+	global valWord
+	isWord[node] = True
+	find(node, word)
+	print("tree")
+	for i in tree:
+		print(i)
+	print("current val words", node, word)
+	for i in valWord:
+		print(i)
+	node = 0
+	word = ""
 	return processed_text
-
