96cd98465cf57d079326c77e5aa19500982e3cbf
Jherson <admedinaco@unal.edu.co>
2020-11-28 13:40:53 -0500

    fixing trie

--- my_env/test.py
+++ my_env/test.py
@@ -6,69 +6,66 @@ app = Flask(__name__)
 
 #-------------GLOBAL VARIABLES------------------------------------------------------------
 node = 0 #Number of nodes, the root node is node 0
-tree = [{}] #Tree itself
-valWord = {} #Set of the most frequent valid words
+tree = [] #Tree itself
+valWord = [] #Set of the most frequent valid words
 freq = [] #Frecuency of Node i
 isWord = [] #Boolean value set true if the current node is a complete word
 word = "" #Current word that it's written
-tree.append([{}])
+tree.append({})
 freq.append(1)
 isWord.append(False)
 #-----------------------------------------------------------------------------------------
 
 #------------SEARCHING AND ADDING FUNCTIONS-----------------------------------------------
-def find(curnode, curword): #Searhces for all valid words
+#Searhces for all valid words
+def find(curnode, curword):
   global isWord
-  if isWord[curnode]: #If current node is an actual word
-    valWord[curword] = [freq[curnode], curnode]
-  for nxt in tree[curnode]: #Recursively finds other valid words
-    find(nxt[1], curword + nxt[0])
+  global valWord
+  global Tree
+  global freq
+  if isWord[curnode]:
+    valWord.append({freq[curnode], curnode})
+  else:
+    for c in tree[curnode]:
+      find(i[1], curword + i[0])
   return
-def add(c): #Updates the 'word' by appending 'c' to the end, and searches for new valid words
+#Updates the 'word' by appending 'c' to the end, and searches for new valid words
+def add(c):
   global node
+  global tree
+  global freq
+  global isWord
   global word
-  if not c.isalpha():
-    freq[node] += 1
-    isWord[node] = True
-    word = ""
-    return
+  print(c)
   if c in tree[node]:
     node = tree[node][c]
-    word += c
-  else :
+    isWord[node] += 1
+  else:
     tree[node][c] = node + 1
     node += 1
-    tree.append({})
     freq.append(1)
-    isWord.append(False)
-    word += c
-  valWord.clear()
-  #find(node, word)
+    isWord.append(1)
+    tree.append({})
+  word += c
+  find(node, word)
   return
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-	text = request.form['input']
-	processed_text = text.upper()
-	for c in processed_text:
-		add(c)
-	global node
-	global word
-	global tree
-	global isWord
-	global valWord
-	isWord[node] = True
-	find(node, word)
-	print("tree")
-	for i in tree:
-		print(i)
-	print("current val words", node, word)
-	for i in valWord:
-		print(i)
-	node = 0
-	word = ""
-	return processed_text
+  global tree
+  global node
+  global word
+  global valWord
+  text = request.form['input']
+  processed_text = text.upper()
+  print(tree)
+  for c in processed_text:
+    print(c)
+    add(c)
+  node = 0
+  word = ""
+  return processed_text
